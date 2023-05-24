58c4fcee1f724112fa29094a8f969e0f7830690d
Anees Khan <ak4577438@gmail.com>
2023-05-24 14:10:00 +0500

    initial commit

---
+++ clock.js
@@ -0,0 +1,34 @@
+// setInterval(() =>{
+//     d= new Date();
+//     htime = d.getHours();
+//     mtime = d.getMinutes();
+//     stime = d.getSeconds();
+//     hrotation = 30*htime + mtime/2;
+//     mrotation = 6*mtime;
+//     srotation = 6*stime;
+
+
+//     hour.style.transform = `rotate(${hrotation}deg)`;
+//     minute.style.transform = `rotate(${mrotation}deg)`;
+//     seconds.style.transform = `rotate(${srotation}deg)`;
+// },1000);
+
+// // setInterval();
+// const deg=6;
+let hour= document.querySelector(".hour");
+let minute=document.querySelector(".minute");
+let seconds=document.querySelector(".second");
+setInterval(
+    function(){
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+   hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+},1000);
